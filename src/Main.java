649534c98a31d65fa53cda0aa4a28346ca1aa102
Frexor <mateuszj93@gmail.com>
2020-11-13 15:12:33 +0100

    Convert numbers in an equation within one digit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import com.sun.pisces.PiscesRenderer;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -10,9 +8,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedImage img = new BufferedImage(500, 200, BufferedImage.TYPE_INT_RGB);
         Scanner scanner = new Scanner(System.in);
         String equation = scanner.next();
+        String[] quizzes = correctEquationToQuizzes(equation);
+        for (String quiz : quizzes) {
+            System.out.println(quiz);
+        }
+    }
+
+    static void equationInImage(String equation) throws IOException {
+        BufferedImage img = new BufferedImage(500, 200, BufferedImage.TYPE_INT_RGB);
         char firstNumber = equation.charAt(0);
         char firstSymbol = equation.charAt(1);
         char secondNumber = equation.charAt(2);
@@ -148,5 +153,53 @@ public class Main {
             }
         }
         return result;
+    }
+
+    static String[] correctEquationToQuizzes(String equation) {
+
+        char[] equationChar = equation.toCharArray();
+        String[] tempResults = new String[100];
+
+
+//        char[][] canAddTo = new char[255][];
+//        canAddTo['1'] = new char[]{'7'};
+//        canAddTo['3'] = new char[]{'9'};
+//        canAddTo['5'] = new char[]{'6', '9'};
+//        canAddTo['6'] = new char[]{'8'};
+//        canAddTo['9'] = new char[]{'8'};
+//        canAddTo['0'] = new char[]{'8'};
+//        canAddTo['-'] = new char[]{'+'};
+//
+//        char[][] canTakeFrom = new char[255][];
+//        canTakeFrom['7'] = new char[]{'1'};
+//        canTakeFrom['8'] = new char[]{'0'};
+//        canTakeFrom['9'] = new char[]{'3', '5'};
+//        canTakeFrom['+'] = new char[]{'-'};
+
+        char[][] canChangePosition = new char[255][];
+        canChangePosition['0'] = new char[]{'6', '9'};
+        canChangePosition['1'] = new char[]{};
+        canChangePosition['2'] = new char[]{'3'};
+        canChangePosition['3'] = new char[]{'5', '2'};
+        canChangePosition['4'] = new char[]{};
+        canChangePosition['5'] = new char[]{'3'};
+        canChangePosition['6'] = new char[]{'0', '9'};
+        canChangePosition['7'] = new char[]{};
+        canChangePosition['8'] = new char[]{};
+        canChangePosition['9'] = new char[]{'0', '6'};
+
+        int k = 0;
+        for (int i = 0; i < 5; i += 2) {
+            for (int j = 0; j < canChangePosition[equation.charAt(i)].length; j++) {
+                char temp = equation.charAt(i);
+                equationChar[i] = canChangePosition[equation.charAt(i)][j];
+                tempResults[k++] = new String(equationChar);
+                equationChar[i] = temp;
+            }
+        }
+        String[] results = new String[k];
+        System.arraycopy(tempResults, 0, results, 0, k);
+
+        return results;
     }
 }
