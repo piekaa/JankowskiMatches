a282b94981c5b82419a7ea8879cfc02dfa78a4fd
Frexor <mateuszj93@gmail.com>
2020-12-02 16:00:39 +0100

    changing position allPossibleMatchMoves method and transferring some code to constructor and class

--- src/EquationGenerator.java
+++ src/EquationGenerator.java
@@ -1,5 +1,52 @@
 public class EquationGenerator {
+
     String[] correctEquations;
+    String[] tempResults = new String[100];
+    char[][] canChangePosition = new char[255][];
+    char[][] canTakeFrom = new char[255][];
+    char[][] canAddTo = new char[255][];
+
+    EquationGenerator() {
+        canChangePosition['0'] = new char[]{'6', '9'};
+        canChangePosition['1'] = new char[]{};
+        canChangePosition['2'] = new char[]{'3'};
+        canChangePosition['3'] = new char[]{'5', '2'};
+        canChangePosition['4'] = new char[]{};
+        canChangePosition['5'] = new char[]{'3'};
+        canChangePosition['6'] = new char[]{'0', '9'};
+        canChangePosition['7'] = new char[]{};
+        canChangePosition['8'] = new char[]{};
+        canChangePosition['9'] = new char[]{'0', '6'};
+
+        canTakeFrom['0'] = new char[]{};
+        canTakeFrom['1'] = new char[]{};
+        canTakeFrom['2'] = new char[]{};
+        canTakeFrom['3'] = new char[]{};
+        canTakeFrom['4'] = new char[]{};
+        canTakeFrom['5'] = new char[]{};
+        canTakeFrom['6'] = new char[]{};
+        canTakeFrom['7'] = new char[]{'1'};
+        canTakeFrom['8'] = new char[]{'0'};
+        canTakeFrom['9'] = new char[]{'3', '5'};
+        canTakeFrom['+'] = new char[]{'-'};
+        canTakeFrom['-'] = new char[]{};
+        canTakeFrom['='] = new char[]{};
+
+        canAddTo['0'] = new char[]{};
+        canAddTo['1'] = new char[]{'7'};
+        canAddTo['2'] = new char[]{};
+        canAddTo['3'] = new char[]{'9'};
+        canAddTo['4'] = new char[]{};
+        canAddTo['5'] = new char[]{'6', '9'};
+        canAddTo['6'] = new char[]{'8'};
+        canAddTo['7'] = new char[]{};
+        canAddTo['8'] = new char[]{};
+        canAddTo['9'] = new char[]{'8'};
+        canAddTo['0'] = new char[]{'8'};
+        canAddTo['+'] = new char[]{};
+        canAddTo['-'] = new char[]{'+'};
+        canAddTo['='] = new char[]{};
+    }
 
     void generateCorrectEquations() {
         correctEquations = new String[110];
@@ -16,22 +63,20 @@ public class EquationGenerator {
         }
     }
 
+    String[] allPossibleMatchMoves(String equation) {
+
+        int movingMatchInCharacter = correctEquationToQuizzes(equation).length;
+        int movingMatchBetweenCharacters = changeMatchPositionBetweenCharacters(equation).length;
+        String[] results = new String[movingMatchInCharacter + movingMatchBetweenCharacters];
+
+        System.arraycopy(correctEquationToQuizzes(equation), 0, results, 0, movingMatchInCharacter);
+        System.arraycopy(changeMatchPositionBetweenCharacters(equation), 0, results, movingMatchInCharacter, movingMatchBetweenCharacters);
+
+        return results;
+    }
+
     String[] correctEquationToQuizzes(String equation) {
-
         char[] equationChar = equation.toCharArray();
-        String[] tempResults = new String[100];
-
-        char[][] canChangePosition = new char[255][];
-        canChangePosition['0'] = new char[]{'6', '9'};
-        canChangePosition['1'] = new char[]{};
-        canChangePosition['2'] = new char[]{'3'};
-        canChangePosition['3'] = new char[]{'5', '2'};
-        canChangePosition['4'] = new char[]{};
-        canChangePosition['5'] = new char[]{'3'};
-        canChangePosition['6'] = new char[]{'0', '9'};
-        canChangePosition['7'] = new char[]{};
-        canChangePosition['8'] = new char[]{};
-        canChangePosition['9'] = new char[]{'0', '6'};
 
         int k = 0;
         for (int i = 0; i < 5; i += 2) {
@@ -49,24 +94,7 @@ public class EquationGenerator {
     }
 
     String[] changeMatchPositionBetweenCharacters(String equation) {
-
         char[] equationChar = equation.toCharArray();
-        String[] tempResults = new String[100];
-
-        char[][] canTakeFrom = new char[255][];
-        canTakeFrom['0'] = new char[]{};
-        canTakeFrom['1'] = new char[]{};
-        canTakeFrom['2'] = new char[]{};
-        canTakeFrom['3'] = new char[]{};
-        canTakeFrom['4'] = new char[]{};
-        canTakeFrom['5'] = new char[]{};
-        canTakeFrom['6'] = new char[]{};
-        canTakeFrom['7'] = new char[]{'1'};
-        canTakeFrom['8'] = new char[]{'0'};
-        canTakeFrom['9'] = new char[]{'3', '5'};
-        canTakeFrom['+'] = new char[]{'-'};
-        canTakeFrom['-'] = new char[]{};
-        canTakeFrom['='] = new char[]{};
 
         int k = 0;
         for (int i = 0; i < 5; i++) {
@@ -89,25 +117,7 @@ public class EquationGenerator {
     }
 
     String[] addMatchToCharacter(String equation, int position) {
-
         char[] equationChar = equation.toCharArray();
-        String[] tempResults = new String[100];
-
-        char[][] canAddTo = new char[255][];
-        canAddTo['0'] = new char[]{};
-        canAddTo['1'] = new char[]{'7'};
-        canAddTo['2'] = new char[]{};
-        canAddTo['3'] = new char[]{'9'};
-        canAddTo['4'] = new char[]{};
-        canAddTo['5'] = new char[]{'6', '9'};
-        canAddTo['6'] = new char[]{'8'};
-        canAddTo['7'] = new char[]{};
-        canAddTo['8'] = new char[]{};
-        canAddTo['9'] = new char[]{'8'};
-        canAddTo['0'] = new char[]{'8'};
-        canAddTo['+'] = new char[]{};
-        canAddTo['-'] = new char[]{'+'};
-        canAddTo['='] = new char[]{};
 
         int k = 0;
         for (int i = 0; i < 5; i++) {
@@ -125,18 +135,6 @@ public class EquationGenerator {
 
         return results;
 
-    }
-
-    String[] allPossibleMatchMoves(String equation) {
-
-        int movingMatchInCharacter = correctEquationToQuizzes(equation).length;
-        int movingMatchBetweenCharacters = changeMatchPositionBetweenCharacters(equation).length;
-        String[] results = new String[movingMatchInCharacter + movingMatchBetweenCharacters];
-
-        System.arraycopy(correctEquationToQuizzes(equation), 0, results, 0, movingMatchInCharacter);
-        System.arraycopy(changeMatchPositionBetweenCharacters(equation), 0, results, movingMatchInCharacter, movingMatchBetweenCharacters);
-
-        return results;
     }
 
 }
