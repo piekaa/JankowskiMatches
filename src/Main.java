2383418c5fc086d185e7643b6a5a1273fdf50867
Frexor <mateuszj93@gmail.com>
2020-12-15 21:01:59 +0100

    adding code that generates all posible quizzes

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        String equation = scanner.next();
-        Image image = new Image();
-        image.drawEquation(equation);
-        image.saveToPng("matches.png");
+//        Scanner scanner = new Scanner(System.in);
+//        String equation = scanner.next();
+//        Image image = new Image();
+//        image.drawEquation(equation);
+//        image.saveToPng("matches.png");
+//        String[] quizzes = equationGenerator.allPossibleMatchMoves(equation);
+//        for (String quiz : quizzes) {
+//            System.out.println(quiz);
+//        }
         EquationGenerator equationGenerator = new EquationGenerator();
-        String[] quizzes = equationGenerator.allPossibleMatchMoves(equation);
-        for (String quiz : quizzes) {
-            System.out.println(quiz);
+        Map<String, Set<String>> quizzes = new HashMap<>();
+        equationGenerator.generateCorrectEquations();
+        String[] correctEquations = equationGenerator.correctEquations;
+        for (String correctEquation : correctEquations) {
+            for (String quiz : equationGenerator.allPossibleMatchMoves(correctEquation)) {
+                if (!quizzes.containsKey(quiz)) {
+                    Set<String> newSet = new HashSet<>();
+                    quizzes.put(quiz, newSet);
+                }
+                Set<String> s = quizzes.get(quiz);
+                s.add(correctEquation);
+            }
         }
     }
-
 }
